caa02bcd5fd76894380979f80ef5542196ce86f3
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2024-01-11 21:13:27 -0500

    test

---
+++ forecast_model_merge.R
@@ -0,0 +1,22 @@
+forecast_model_merge <- function(folder, model_id){
+
+  library("tidyverse")
+
+  dir.create("forecast_files")
+  setwd("forecast_files")
+  
+  for (i in 1:34){
+    forecast_file <- paste0("aquatics","-",model_id,"-",i,".csv.gz")
+    FaaSr::faasr_get_file(local_file=forecast_file, remote_folder=folder, remote_file=forecast_file)
+  }
+
+  file_lists <- list.files()
+  result <- map_dfr(file_lists, read_csv)
+  
+  file_date <- Sys.Date() #forecast$reference_datetime[1]
+  forecast_file <- paste0("aquatics","-",file_date,"-",model_id,".csv.gz")
+  
+  write_csv(result, forecast_file)
+  FaaSr::faasr_put_file(local_file=forecast_file, remote_folder=folder, remote_file=forecast_file)
+  
+}
